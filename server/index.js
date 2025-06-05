733789412ec9a4bb3fcc9a4c294a0ea6185e0c52
sebasrengi <sebas.rengi@hotmail.com>
2025-06-04 19:45:34 -0500

    test2

--- server/index.js
+++ server/index.js
@@ -1,44 +1,47 @@
-const puppeteer = require ("puppeteer")
+const puppeteer = require("puppeteer")
 
-(async () => {
+;(async () => {
     const URL = "https://www.amazon.com/s?k=medias+de+compresi%C3%B3n+para+mujer";
     const browser = await puppeteer.launch({
         headless: false,
     });
     const page = await browser.newPage();
-    await page.goto(URL, { waituntil: "networkidle2"});
-    const title = await page.title()
-    console.log('titulo de la pagina : ${title}')
+    await page.goto(URL, { waitUntil: "networkidle2"});
+    const title = await page.title();
+    console.log('titulo de la pagina : ${title}');
 
     let products = [];
     let nextPage = true;
-     while (nextPage) { 
-       const products= await pageXOffset.evalute (()=> {
-            const products = Array.from(document.querySelectorAll('.puis-card-container.s-card-container'))
+     while (nextPage) {
+       const newProducts = await page.evaluate(() => {
+            const products = Array.from(document.querySelectorAll('.puis-card-container.s-card-container'));
 
-           return products.map(products =>{
-                const title = products.querySelector('.a-text-normal').innerText
-                const priceWhole = products.querySelector('.a-price-whole').innerText
-                const priceFraction = products.querySelector('.a-price-fraction').innerText
-            
+           return products.map(product => {
+                const title = product.querySelector('.a-text-normal').innerText;
+                const priceWhole = product.querySelector('.a-price-whole').innerText;
+                const priceFraction = product.querySelector('.a-price-fraction').innerText;
+
                 if (!priceWhole || !priceFraction) {
-                    return
+                    return null;
                 }
-                console.log(title)
-                console.log(priceWhole + '.'+ priceFraction)
-                console.log(priceFraction)
-            })
-        })
-        products = {...products, ...newProducts}
-        await page.evalute(() => {
-            const nextButton = document.querySelector('.s-pagination-next')
+                console.log(title);
+                console.log(priceWhole + '.' + priceFraction);
+                console.log(priceFraction);
+                return {
+                  title,
+                  price: priceWhole + '.' + priceFraction
+                };
+            });
+        });
+        products = {...products, ...newProducts};
+        await page.evaluate(async () => {
+            const nextButton = document.querySelector('.s-pagination-next');
             if (nextButton) {
-                nextButton.click()
-                return true
+                nextButton.click();
+                return true;
+            } else {
+              return false;
             }
-            return false
-        })
+        });
     }
 })();
-
-   
